b9d485f411331985d7425ccc07a3d4656f8dfb2d
Justaplainoldusername <maxine.e.mccabe@gmail.com>
2025-09-22 19:03:34 +1000

    Week 8 - IA1 - Behavioural Design
    Added the Behaviour Design pages include the css and js.

---
+++ work_phase_3/js/behavioural_design.js
@@ -0,0 +1,23 @@
+import { postFormData } from './modules/postFormData.js';
+            
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('community-form');
+    const feedback = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        feedback.textContent = 'Submitting...';
+        const { success, data } = await postFormData(form, 'https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community/', {
+            'student_number': 's47940398',
+            'uqcloud_zone_id': 'e9ee3aeb',
+        });
+
+        if (success) {
+            feedback.textContent = data.message;
+            form.reset();
+        } else {
+            feedback.textContent = data.message || 'Something went wrong.';
+        }
+    });
+});
